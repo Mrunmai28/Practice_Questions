5e9cf0fbcf6653e5a963f49ad14f98c44b470472
Mrunmai Lokhande <mrunmailokhande09@gmail.com>
2024-01-30 01:34:41 +0530

    First Commit

---
+++ Arithmetic.java
@@ -0,0 +1,25 @@
+class Arithmetic {
+    public static void main(String[] args) {
+        int result;
+        int num1 = 10;
+        int num2 = 5;
+
+        // Addition
+        result = num1 + num2;
+        System.out.println("Sum: " + result);  
+
+        // Subtraction
+        result = num1 - num2;
+        System.out.println("Subtraction: " + result); 
+        // Multiplication
+        result = num1 * num2;
+        System.out.println("Multiplication: " + result); 
+        // Division
+        result = num1 / num2;
+        System.out.println("Quotient: " + result);  
+
+        // Modulus
+        result = num1 % num2;
+        System.out.println("Remainder: " + result);  
+}
+}
